ea98bcfc26331e465afc1c356c2a73d2e659fdd3
Rane Gowan <rane.gowan@red-badger.com>
2021-09-22 18:16:07 +0100

    added button event handlers

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#business").on('click', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $('#business-section').offset().top - 100
+    }, 100);
+  });
+
+  $("#influencer").on('click', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $('#influencer-section').offset().top - 100
+    }, 100);
+  });
+});
